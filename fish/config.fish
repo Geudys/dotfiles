0cef8994c85c2fa14a45a5b4330aa73d09347fbb
Geudys <geudysperezstgo@gmail.com>
2025-12-04 19:40:44 -0400

    add-function-fish
    add: function superfile in fish

--- fish/config.fish
+++ fish/config.fish
@@ -165,4 +165,23 @@ function y
     rm -f -- "$tmp"
 end
 
+# Sincronizar Superfile
+function s
+    set os $(uname -s)
+
+    if test "$os" = "Linux"
+        set spf_last_dir "$HOME/.local/state/superfile/lastdir"
+    end
+
+    if test "$os" = "Darwin"
+        set spf_last_dir "$HOME/Library/Application Support/superfile/lastdir"
+    end
+
+    command spf $argv
+
+    if test -f "$spf_last_dir"
+        source "$spf_last_dir"
+        rm -f -- "$spf_last_dir" >> /dev/null
+    end
+end
 set -gx PATH $PATH ./node_modules/.bin
